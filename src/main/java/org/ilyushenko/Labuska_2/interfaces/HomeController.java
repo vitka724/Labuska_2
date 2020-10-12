9c68fdbde2e058d0babf4a84b54289e260a2bf38
Vitaliya <vitka724@gmail.com>
2020-10-12 10:27:39 +0300

    Commit 5

--- src/main/java/org/ilyushenko/Labuska_2/interfaces/HomeController.java
+++ src/main/java/org/ilyushenko/Labuska_2/interfaces/HomeController.java
@@ -3,7 +3,9 @@ package org.ilyushenko.Labuska_2.interfaces;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -11,10 +13,11 @@ public class HomeController {
     private org.springframework.context.annotation.AnnotationConfigApplicationContext context;
 
     @RequestMapping(value = "/")
-    public String home( Model model){
+    public String home(@RequestParam(value = "name", required = false, defaultValue = "stranger") String name,
+                       Model model){
         context = new AnnotationConfigApplicationContext(JavaConfig.class);
         Sender sender = context.getBean( "wirelessSenderImpl", Sender.class );
-        model.addAttribute( "msg",  sender.sendMessage( "Message from Java config" ));
+        model.addAttribute( "msg",  sender.sendMessage( "Hello " + name + "!" ));
         return "test";
     }
 }
