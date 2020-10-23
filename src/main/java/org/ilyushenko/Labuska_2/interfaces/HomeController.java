71413b4bbf158a2710be68cf82ab082ab3726552
Vitaliya <vitka724@gmail.com>
2020-10-24 02:14:53 +0300

    Commit 8

--- src/main/java/org/ilyushenko/Labuska_2/interfaces/HomeController.java
+++ src/main/java/org/ilyushenko/Labuska_2/interfaces/HomeController.java
@@ -1,13 +1,14 @@
 package org.ilyushenko.Labuska_2.interfaces;
 
+
+import org.ilyushenko.Labuska_2.interfaces.entity.User;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Collection;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -20,5 +21,14 @@ public class HomeController {
         Sender sender = context.getBean( "wirelessSenderImpl", Sender.class );
         model.addAttribute( "msg",  sender.sendMessage( "Hello " + name + "!" ));
         return "test";
+    }
+    @RequestMapping (value = "/")
+    public String getUsers(Model model){
+        Collection<User> users = List.of(
+        new User( "John", "Smith","js@asd.com"),
+                new User("Mikle", "Johnson", "mi@asd.com")
+        );
+        model.addAttribute("users", users);
+        return "users";
     }
 }
