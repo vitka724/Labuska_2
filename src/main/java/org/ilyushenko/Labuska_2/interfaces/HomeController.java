653903bf72d6d5916e468cb89502f8f968344266
Vitaliya <vitka724@gmail.com>
2020-10-24 03:10:10 +0300

    Commit 9

--- src/main/java/org/ilyushenko/Labuska_2/interfaces/HomeController.java
+++ src/main/java/org/ilyushenko/Labuska_2/interfaces/HomeController.java
@@ -5,30 +5,39 @@ import org.ilyushenko.Labuska_2.interfaces.entity.User;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 @Controller
 public class HomeController {
     private org.springframework.context.annotation.AnnotationConfigApplicationContext context;
-
+    private Collection<User> users = new ArrayList<>();
     @RequestMapping(value = "/{name}")
     public String home(@PathVariable("name") String name,
-                       Model model){
+                       Model model) {
         context = new AnnotationConfigApplicationContext(JavaConfig.class);
-        Sender sender = context.getBean( "wirelessSenderImpl", Sender.class );
-        model.addAttribute( "msg",  sender.sendMessage( "Hello " + name + "!" ));
+        Sender sender = context.getBean("wirelessSenderImpl", Sender.class);
+        model.addAttribute("msg", sender.sendMessage("Hello " + name + "!"));
         return "test";
     }
-    @RequestMapping (value = "/")
-    public String getUsers(Model model){
-        Collection<User> users = List.of(
-        new User( "John", "Smith","js@asd.com"),
-                new User("Mikle", "Johnson", "mi@asd.com")
-        );
-        model.addAttribute("users", users);
+
+    @RequestMapping(value = "/users")
+    public String getUsers(Model model) {
+        model.addAttribute("users", users );
         return "users";
     }
+    @GetMapping(value = "/")
+    public String getSingUp() {
+        return "sign_up";
+    }
+    @PostMapping(value = "/")
+    public  String getSingUp(@ModelAttribute User user){
+        users.add(user);
+        return  "redirect:/users";
+    }
+
+
 }
